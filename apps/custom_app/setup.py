f6878c684564a440c879887cb5bda7e9a0390daa
PORTATIL-JUAN-R\Alejandro <alejandromejiasramirez2006@gmail.com>
2026-05-09 22:15:57 +0100

    fix: stabilize docker infrastructure and migrate app to standard path

--- apps/custom_app/setup.py
+++ apps/custom_app/setup.py
@@ -1,12 +1,1 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="custom_app",
-    version="0.0.1",
-    description="App personalizada para carga de datos",
-    author="Zyroks0906",
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=("frappe",),
-)
+from setuptools import setup, find_packages; setup(name="custom_app", version="0.0.1", packages=find_packages())
